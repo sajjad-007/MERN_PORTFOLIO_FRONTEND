16bac5315202312cdd78402a4ca6738476dd9c24
sajjad-007 <sajjadhossain8123@gmail.com>
2025-08-20 11:34:42 +0600

    portfolio part done

--- src/pages/ProjectView.jsx
+++ src/pages/ProjectView.jsx
@@ -1,9 +1,149 @@
-import React from 'react'
+import { Button } from '@/components/ui/button';
+import { Label } from '@/components/ui/label';
+
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const ProjectView = () => {
-  return (
-    <div>ProjectView</div>
-  )
-}
+  const { id } = useParams();
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgPreview, setImagePreview] = useState('');
+  const [gitRepoLink, setGitRepoLink] = useState('');
+  const [projectLink, setProjectLink] = useState('');
+  const [stack, setStack] = useState('');
+  const [technologies, setTechnologies] = useState('');
+  const [deployed, setDeployed] = useState('');
 
-export default ProjectView
+  useEffect(() => {
+    const getSingleProject = async () => {
+      await axios
+        .get(`http://localhost:4000/api/v1/project/get/${id}`, {
+          withCredentials: true,
+        })
+        .then(res => {
+          setTitle(res?.data?.findProject?.title);
+          setDescription(res?.data?.findProject?.description);
+          setImagePreview(
+            res?.data?.findProject?.projectImage &&
+              res?.data?.findProject?.projectImage?.url
+          );
+          setStack(res?.data?.findProject?.stack);
+          setDeployed(res?.data?.findProject?.deployed);
+          setTechnologies(res?.data?.findProject?.technologies);
+          setGitRepoLink(res?.data?.findProject?.gitRepoLink);
+          setProjectLink(res?.data?.findProject?.projectLink);
+        })
+        .catch(error => {
+          toast.error(error.response.data.message);
+        });
+    };
+    getSingleProject();
+  }, []);
+
+  const descriptionSlice = description.split(', ');
+  const technologiesSlice = technologies.split(', ');
+  return (
+    <div className="flex justify-center items-center min-h-[100vh] sm:gap-4 sm:py-4 sm:pl-14">
+      <div className="w-[100%] px-5 md:w-[650px]">
+        <div className="space-y-12">
+          <div className="border-b border-gray-900/10">
+            {/* title */}
+            <div className="flex items-center justify-between py-4">
+              <h2 className="font-semibold leading-7 text-3xl text-center ">
+                {title}
+              </h2>
+              <Link to={'/'}>
+                <Button className="cursor-pointer capitalize ">
+                  Return to portfolio
+                </Button>
+              </Link>
+            </div>
+            <div className="flex flex-col gap-5">
+              {/* Image  */}
+              <div className="col-span-full">
+                <div className="flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-4">
+                  <div className="text-center">
+                    {imgPreview ? (
+                      <img
+                        src={
+                          imgPreview
+                            ? imgPreview
+                            : '/DASHBOARD/public/react.png'
+                        }
+                        alt="preview image"
+                        className="w-full h-auto object-cover"
+                      />
+                    ) : (
+                      <svg
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        aria-hidden="true"
+                        className="mx-auto h-50 w-[300px] text-gray-300"
+                      >
+                        <path
+                          d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z"
+                          clipRule="evenodd"
+                          fillRule="evenodd"
+                        />
+                      </svg>
+                    )}
+                  </div>
+                </div>
+              </div>
+              {/* Description */}
+              <div className="w-full sm:col-span-4">
+                <p className="text-2xl mb-2 capitalize">Description:</p>
+                <ul className="mt-2 list-disc ml-4">
+                  {descriptionSlice.map((item, index) => (
+                    <li key={index}>{item}</li>
+                  ))}
+                </ul>
+              </div>
+              {/* technologies */}
+              <div className="w-full sm:col-span-4 ">
+                <p className="text-2xl mb-2 capitalize">
+                  technologies used in this project:
+                </p>
+
+                <ul className="mt-2 list-disc ml-4">
+                  {technologiesSlice.map((item, index) => (
+                    <li key={index}>{item}</li>
+                  ))}
+                </ul>
+              </div>
+
+              {/* stack */}
+              <div className="w-full sm:col-span-4">
+                <p className="text-2xl mb-2">Stack:</p>
+                <p>{stack}</p>
+              </div>
+              {/* Deployed */}
+              <div className="w-full sm:col-span-4">
+                <p className="text-2xl mb-2">Deployed:</p>
+                <p>{deployed}</p>
+              </div>
+              {/* Github link*/}
+              <div className="w-full sm:col-span-4">
+                <p className="text-2xl mb-2">Github Repository Link:</p>
+                <Link className="text-sky-700" target="_blank" to={gitRepoLink}>
+                  {gitRepoLink}
+                </Link>
+              </div>
+              <div className="w-full sm:col-span-4">
+                <p className="text-2xl mb-2">Project Link:</p>
+                <Link className="text-sky-700" target="_blank" to={projectLink}>
+                  {projectLink}
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectView;
